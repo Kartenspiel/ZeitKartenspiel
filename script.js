31898007f8dce79b5a78e8a0fa2e0b3b5463465c
AminLakhal <64310291+AminLakhal@users.noreply.github.com>
2024-12-10 21:33:58 +0100

    Array

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-const soundCounts = Array(10).fill(0); // Zähler für die Anzahl der Abspielversuche
-const maxPlays = 3; // Maximale Anzahl der Abspielversuche pro Sound
+const soundCounts = Array(10).fill(0);
+const maxPlays = 3;
 
-// Array mit den tatsächlichen Dateinamen der Sounds
+// SOUNDARRAY
 const soundFiles = [
-    'Pac-Man Waka Waka Seamless Loop.mp3', // Ersetze durch den tatsächlichen Namen
-    'Microsoft Windows XP Startup Sound.mp3', // Ersetze durch den tatsächlichen Namen
-    'Mario Jump Sound Effect.mp3', // Ersetze durch den tatsächlichen Namen
-    'GZSZ Logo 1992.mp3', // Ersetze durch den tatsächlichen Namen
-    'iMessage sound effects.mp3', // Ersetze durch den tatsächlichen Namen
-    'MSN Messenger sound.mp3', // Ersetze durch den tatsächlichen Namen
-    '[TAS] 3D Pinball Space Cadet (Wii) - One Mission Complete In 8.87s.mp3', // Ersetze durch den tatsächlichen Namen
-    'The Pink Panther Intro.mp3', // Ersetze durch den tatsächlichen Namen
-    'THX intro.mp3', // Ersetze durch den tatsächlichen Namen
-    '11880 da werden sie geholfen TV Webung.mp3' // Ersetze durch den tatsächlichen Namen
-];
+        'Pac-Man Waka Waka Seamless Loop.mp3',
+        'Mario Jump Sound Effect.mp3',
+        'Microsoft Windows XP Startup Sound.mp3',
+        'THX intro.mp3',
+        'The Pink Panther Intro.mp3',
+        'MSN Messenger sound.mp3',
+        '[TAS] 3D Pinball Space Cadet (Wii) - One Mission Complete In 8.87s.mp3',
+        'NOKIA RINGTONE [1994].mp3',
+        'Upps! Die Pannenshow Intro.mp3',
+        'GZSZ Logo 1992.mp3'
 
-// Funktion zum Abspielen des Sounds
+
+    ]
+    // Pllay
 function playSound(buttonNumber) {
     if (soundCounts[buttonNumber - 1] < maxPlays) {
-        const audio = new Audio(`Sounds/${soundFiles[buttonNumber - 1]}`); // Pfad zur Sounddatei
-        audio.play(); // Sound abspielen
-        soundCounts[buttonNumber - 1]++; // Zähler erhöhen
+        const audio = new Audio(`Sounds/${soundFiles[buttonNumber - 1]}`);
+        audio.play();
+        soundCounts[buttonNumber - 1]++;
     } else {
         alert(`Der Sound für Button ${buttonNumber} wurde bereits ${maxPlays} Mal abgespielt.`);
     }
 }
 
-// Funktion zum Zurücksetzen der Zähler
+// Reset
 function resetCounters() {
     for (let i = 0; i < soundCounts.length; i++) {
-        soundCounts[i] = 0; // Zähler zurücksetzen
+        soundCounts[i] = 0;
     }
-    alert("Die Zähler wurden zurückgesetzt."); // Bestätigung anzeigen
+    alert("Die Zähler wurden zurückgesetzt.");
 }
